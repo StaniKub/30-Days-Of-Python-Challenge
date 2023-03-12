8974e69bc60d8b66c04891f05054986545bc90fd
StaniKub <stanislav.kubernat@gmail.com>
2023-03-12 20:13:54 +0100

    tomorrow is another day

---
+++ day_3/day_3.py
@@ -0,0 +1,9 @@
+age = 29
+height = 178.0
+complex_number = 3 + 2j
+
+# Area of Triangle calculation
+base = input('Enter base of the triangle: ')
+height = input('Enter height of the triangle: ')
+area = 0.5 * int(base) * int(height)
+print(f'Area of your triangle is {area}')
